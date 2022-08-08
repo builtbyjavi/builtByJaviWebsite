802622c789a85a59eadb42bd56888ed8935c5439
builtbyjavi <71475644+builtbyjavi@users.noreply.github.com>
2022-08-08 11:46:50 -0700

    updated desktop layout

--- original/src/components/Bio.js
+++ original/src/components/Bio.js
@@ -33,51 +33,52 @@ function Bio({
 
                 </div>
                 
+                <div className="bio-wrapper">
+                    <div className="bio-container">
+                        <div class="about-div">
+                            <h2 id="about-text">About</h2>
+                        </div>
+                        <div className="about-row-reverse">
+                            <div className="bio-img-wrapper-1">
+                                <img src={img2} alt={alt2} className="bio-img-1">
+                                </img>
+                            </div>
+                            <div className="bio-p-1">
+                                <h3 className="med-text">
+                                    Born and raised in Salinas, CA with roots in Chavinda, Michoacan, Mexico.
+                                    I come from a working people, and have always been fascinated with using
+                                    my hands to create. In recent years this passion has shifted towards a 
+                                    digital medium. Whether it is programming, design, music production,
+                                    or digital art, I am fixated on bringing ideas to life to be enjoyed
+                                    by a global audience.
+                                </h3>
+                            </div>
+                            
+                        </div>
 
-                <div className="bio-container">
-                    <div class="about-div">
-                        <h2 id="about-text">About</h2>
-                    </div>
-                    <div className="about-row-reverse">
-                        <div className="bio-img-wrapper-1">
-                            <img src={img2} alt={alt2} className="bio-img-1">
-                            </img>
+                        <div className="about-row extra-space-bottom">
+                            <div className="bio-img-wrapper-2">
+                                <img src={img3} alt={alt3} className="bio-img-2">
+                                </img>  
+                            </div>
+                            <div className="bio-p-2">
+                                <p className="sm-text">
+                                    I received a Bachelors of Arts degree in Data Science with an emphasis
+                                    in Cognition from the University of California, Berkeley in 2020. I 
+                                    have expanded upon the fundamentals of programming and statistics
+                                    earned through the nation's #1 program, and incorporated that knowledge 
+                                    with my love of digital media which has led me to my current trajectory 
+                                    as a Front-End Software Engineer. I specialize in end-to-end
+                                    product development, branding, and overthinking.
+                                    Hit me up if you would like to collaborate, I am always open to
+                                    making new friends.
+                                </p>
+                            </div>
+                            
                         </div>
-                        <div className="bio-p-1">
-                            <h3 className="med-text">
-                                Born and raised in Salinas, CA with roots in Chavinda, Michoacan, Mexico.
-                                I come from a working people, and have always been fascinated with using
-                                my hands to create. In recent years this passion has shifted towards a 
-                                digital medium. Whether it is programming, design, music production,
-                                or digital art, I am fixated on bringing ideas to life to be enjoyed
-                                by a global audience.
-                            </h3>
-                        </div>
-                        
-                    </div>
 
-                    <div className="about-row extra-space-bottom">
-                        <div className="bio-img-wrapper-2">
-                            <img src={img3} alt={alt3} className="bio-img-2">
-                            </img>  
-                        </div>
-                        <div className="bio-p-2">
-                            <p className="sm-text">
-                                I received a Bachelors of Arts degree in Data Science with an emphasis
-                                in Cognition from the University of California, Berkeley in 2020. I 
-                                have expanded upon the fundamentals of programming and statistics
-                                earned through the nation's #1 program, and incorporated that knowledge 
-                                with my love of digital media which has led me to my current trajectory 
-                                as a Front-End Software Engineer. I specialize in end-to-end
-                                product development, branding, and overthinking.
-                                Hit me up if you would like to collaborate, I am always open to
-                                making new friends.
-                            </p>
-                        </div>
-                        
                     </div>
-
-                </div>
+            </div>
                 
                 
             </div>
